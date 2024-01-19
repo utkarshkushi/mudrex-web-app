e6fd2127fcaaf8273e3bfa6e4473a6e778f99768
utkarshkuhsi <utkarshsuneela@gmail.com>
2024-01-19 23:35:41 +0530

    rectifying git blunder

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,10 +22,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
       <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-    </ClerkProvider>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,53 +2,43 @@
 
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
-<<<<<<< HEAD
-import home from "./home/page";
-=======
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
->>>>>>> bf549ec27219fd18ce21ae76518987c1d59d067a
 
 export default function Home() {
 
 
-  
+
 
   return (
-<<<<<<< HEAD
-    <div>
-    
-      Landing Page
-=======
     <div className="min-h-screen px-5 py-5 bg-gradient-to-b from-[#eddaea] via-[#cdd9f0]  to-[#f2f8fd] to-30%">
       <div>
         <Image height={60} width={180} src='/logo/logo.png' />
       </div>
       <div className="flex flex-col gap-4 justify-center items-center mt-20">
         <div className="flex flex-col gap-4">
-        <h1 className="text-5xl font-bold font-inter text-[#4a34c1] text-center">
-          Make Investments In Crypto
-        </h1>
-        <h1 className="text-[#212021] text-center text-2xl font-inter font-bold">
-          Simple, Safer & Smarter
-        </h1>
+          <h1 className="text-5xl font-bold font-inter text-[#4a34c1] text-center">
+            Make Investments In Crypto
+          </h1>
+          <h1 className="text-[#212021] text-center text-2xl font-inter font-bold">
+            Simple, Safer & Smarter
+          </h1>
         </div>
         <div className="mt-12">
-        <p className="text-[#5f6e79] font-normal font-inter text-md text-center mb-5">
-        LogIn or SignUp
-        </p>
-        <div>
-        <Link href="/home">
-        <button className="border-2 flex justify-center items-center px-4 m-auto py-3 text-md font-inter font-bold bg-[#cdd9f0] rounded-lg">
-              Get Started<ArrowRight className='ml-5 h-5 w-5 ' strokeWidth={3} />
-            </button>
-        </Link> 
+          <p className="text-[#5f6e79] font-normal font-inter text-md text-center mb-5">
+            LogIn or SignUp
+          </p>
+          <div>
+            <Link href="/home">
+              <button className="border-2 flex justify-center items-center px-4 m-auto py-3 text-md font-inter font-bold bg-[#cdd9f0] rounded-lg">
+                Get Started<ArrowRight className='ml-5 h-5 w-5 ' strokeWidth={3} />
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
-      </div>
->>>>>>> bf549ec27219fd18ce21ae76518987c1d59d067a
-      
+
     </div>
   );
 }
