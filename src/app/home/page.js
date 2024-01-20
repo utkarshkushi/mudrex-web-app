6cc624bb3daa2dd22e3e13a34640a6d16acfc4b3
utkarshkuhsi <utkarshsuneela@gmail.com>
2024-01-20 12:25:07 +0530

    design changes to verification section - home page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './_components/Navbar'
 import Verify from './_components/verify'
+import Image from 'next/image'
 const HomePage = () => {
   return (
     <div className='bg-gradient-to-b from-[#eddaea] via-[#cdd9f0]  to-[#f2f8fd] to-30% min-h-screen'>
